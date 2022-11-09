10648ae62e95446dc9df60c694882ce232afa9f7
HorpeX <patrykgrzeszczyk@gmailcom>
2022-11-09 20:44:23 +0100

    photo.jpg

--- auctions_app/urls.py
+++ auctions_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('training/', include('training.urls'))
+    path('main/', include('main.urls'))
 ]
